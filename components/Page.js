3613e68a254238e66b2312f494bf8d448be57ab2
Jan Moons <jan.moons@themasterlabs.com>
2021-11-04 02:13:17 +0100

    some struggles with home

--- components/Page.js
+++ components/Page.js
@@ -1,14 +1,18 @@
 import DynamicComponent from './DynamicComponent'
 import SbEditable from 'storyblok-react'
 
-const Page = ({blok}) => (
-  <SbEditable content={blok}>
-    <main>
-      {blok.body ? blok.body.map((blok) =>
-        <DynamicComponent blok={blok} key={blok._uid} />
-      ) : null}
-    </main>
-  </SbEditable>
-)
+const Page = ({ data }) => {
+  var content = data.story.content;
+  return (
+    <SbEditable content={content}>
+      <main>
+        {content.body ? content.body.map((content) =>
+          <DynamicComponent blok={data} key={content._uid} />
+        ) : null}
+      </main>
+    </SbEditable>
+  )
+}
+
 
 export default Page
