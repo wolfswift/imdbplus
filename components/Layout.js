1f9d0488f5aa9190850aaea0f869bb695952c1bb
Jan Moons <jan.moons@themasterlabs.com>
2021-11-18 21:19:27 +0100

    added social share buttons to products

--- components/Layout.js
+++ components/Layout.js
@@ -7,18 +7,25 @@ import styles from "../styles/Layout.module.scss"
 const Layout = ({ children, locale, locales, data }) => {
 
   let title = "IMDBPlus";
+  let description = "description for page";
   if (data && data.story) {
-    if (data.story.content && data.story.content.title) {
-      title = data.story.content.title;
-    } else if (data.story.name) {
-      title = data.story.name;
+    if (data.story.content) {
+      if (data.story.content.title) {
+        title = data.story.content.title;
+      } else if (data.story.name) {
+        title = data.story.name;
+      }
+      if (data.story.content.short) {
+        description = data.story.content.short;
+      } else if (data.story.content.introduction){
+          description = data.story.content.introduction;
+      }
     }
   }
-  let description = "description for page";
   return (
     <div className={styles.layout}>
-      <Head title={title} description={description} className={styles.head}/>
-      <Navigation locale={locale} locales={locales}  />
+      <Head title={title} description={description} className={styles.head} />
+      <Navigation locale={locale} locales={locales} />
       <NavigationMobile locale={locale} locales={locales} />
       {children}
       <Footer />
--- components/Product.js
+++ components/Product.js
@@ -7,6 +7,8 @@ import RelatedItem from "./RelatedItem"
 import RelatedItemGallery from "./RelatedItemGallery"
 import InPageSlideshow from "./InPageSlideshow"
 import SmallCardList from "./SmallCardList"
+import { FacebookShareButton, FacebookIcon,TwitterShareButton, TwitterIcon, LinkedinShareButton, LinkedinIcon } from 'next-share';
+
 
 
 const Product = ({ data, level }) => {
@@ -21,6 +23,7 @@ const Product = ({ data, level }) => {
   } else {
     var content = data;
   }
+
   return (
     <SbEditable content={content} key={data.uuid}>
       <main>
@@ -29,6 +32,11 @@ const Product = ({ data, level }) => {
           <h1 className={styles.title}>
             {content.title}
           </h1>
+          <div className="sharebar">
+            <FacebookShareButton url={"http://imdbplus.vercel.app/"+data.story.full_slug} quote={"test"} hashtag={'#imdbplus'}><FacebookIcon size={32} round /></FacebookShareButton>
+            <LinkedinShareButton url={"http://imdbplus.vercel.app/"+data.story.full_slug}><LinkedinIcon size={32} round /></LinkedinShareButton>
+            <TwitterShareButton url={"http://imdbplus.vercel.app/"+data.story.full_slug} title={"test"}><TwitterIcon size={32} round /></TwitterShareButton>
+          </div>
           <div className={styles.producthead}>
             <div className={styles.producthead_first}>
               <div className={styles.mainpicture} style={{ backgroundImage: `url("${content.mainpicture.filename}")` }}>
@@ -52,7 +60,7 @@ const Product = ({ data, level }) => {
           <div className={styles.description}>
             <div className={styles.title}>Description</div>
             <div className={styles.content}> {render(content.description)}</div>
-           
+
           </div>
           {movies && movies.length > 0 && <SmallCardList items={movies} title="Related Movies" type="movie"></SmallCardList>}
           {personalities && personalities.length > 0 && <SmallCardList items={personalities} title="Related Stars" type="personality"></SmallCardList>}
