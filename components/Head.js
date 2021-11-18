d7826a1b869aa1ca1a1cdba48f4c3f02ffff53c2
Jan Moons <jan.moons@themasterlabs.com>
2021-11-18 21:49:31 +0100

    added og tags

--- components/Head.js
+++ components/Head.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import NextHead from 'next/head'
 
-const Head = ({ title, description }) => (
+const Head = ({ title, description, ogtitle, ogdescription, ogimage, ogurl, ogsite_name }) => (
   <NextHead>
     <meta charSet="UTF-8" />
     <title>{title || ''}</title>
     <meta name="description" content={description || ''} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:url" content={ogurl} key="ogurl" />
+    <meta property="og:image" content={ogimage} key="ogimage" />
+    <meta property="og:site_name" content={ogsite_name} key="ogsitename" />
+    <meta property="og:title" content={ogtitle} key="ogtitle" />
+    <meta property="og:description" content={ogdescription} key="ogdesc" />
   </NextHead>
 )
 
--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,24 @@ import Navigation from '../components/Navigation'
 import NavigationMobile from './NavigationMobile'
 import Footer from '../components/Footer'
 import styles from "../styles/Layout.module.scss"
+import { useRouter } from 'next/router'
+import { basePath } from '../next.config'
 
 const Layout = ({ children, locale, locales, data }) => {
 
+  const router = useRouter();
+
   let title = "IMDBPlus";
   let description = "description for page";
+  let sitename="IMDB Plus";
+  let url = router.basePath;
+  const hostname="https://imdbplus.vercel.app/"
+  if (typeof window !== 'undefined') {
+    const hostname = window.location.hostname;
+ }
+  let image=hostname+"assets/logo-colored-full.png";
+
+
   if (data && data.story) {
     if (data.story.content) {
       if (data.story.content.title) {
@@ -20,11 +33,19 @@ const Layout = ({ children, locale, locales, data }) => {
       } else if (data.story.content.introduction){
           description = data.story.content.introduction;
       }
+      if(data.story.content.mainpicture){
+        image=data.story.content.mainpicture.filename;
+      }
+      if(data.story.full_slug){
+        url = hostname+data.story.full_slug;
+      }
     }
   }
+
+
   return (
     <div className={styles.layout}>
-      <Head title={title} description={description} className={styles.head} />
+      <Head title={title} description={description} ogtitle={title} ogdescription={description} ogimage={image} ogurl={url} ogsite_name={sitename}  className={styles.head} />
       <Navigation locale={locale} locales={locales} />
       <NavigationMobile locale={locale} locales={locales} />
       {children}
