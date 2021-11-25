4d2d29215f6132d3cc2c34af3e49634014b22b3e
Jan Moons <jan.moons@themasterlabs.com>
2021-11-25 12:10:28 +0100

    update

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,4 +1,5 @@
 import styles from "../styles/Navigation.module.scss"
+import Image from "next/image"
 
 const Navigation = ({ locale, locales }) => {
   const resolveMovies = {
@@ -17,9 +18,9 @@ const Navigation = ({ locale, locales }) => {
     en: 'Shop',
     nl: 'Winkel',
   }
-  let homeurl="/";
-  if(locale!='default'){
-    homeurl = "/"+locale;
+  let homeurl = "/";
+  if (locale != 'default') {
+    homeurl = "/" + locale;
   }
 
   const defaultLocale = locale === 'en' ? '/' : `/${locale}/`
@@ -28,13 +29,13 @@ const Navigation = ({ locale, locales }) => {
       <nav className={styles.navigation} role="navigation">
 
         <div className={styles.navlogo}>
-        <a href={homeurl}>
+          <a href={homeurl}>
             {/* <img
               src="https://a.storyblok.com/f/133261/3039x582/a60d166ec2/logo-colored-full.png/m/200x0"
               alt="IMDBPlus Logo"
               className=""
             /> */}
-            <Image src="/logo-colored-full.png" alt="IMDBPlus Logo" width="128" height="25"/>
+            <Image src="/logo-colored-full.png" alt="IMDBPlus Logo" width="128" height="25" />
           </a>
         </div>
         <div className={styles.navlinkswrapper}>
